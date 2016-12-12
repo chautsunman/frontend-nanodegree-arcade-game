d5acb1b07d3cf26d76eaebe77a08b7a469aa1ccf
Chau Tsun Man <tsunman.chau96@gmail.com>
2016-12-12 11:10:54 +0800

    Update the enemy location

--- js/app.js
+++ js/app.js
@@ -21,6 +21,7 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x += speed * dt;
 };
 
 // Draw the enemy on the screen, required method for game
